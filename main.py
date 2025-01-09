49d3e4886dedc5bfb77d4419e7d0c37fd001f873
Ammar-Cevro <ammar.cevro@gmail.com>
2025-01-09 20:02:31 +0100

    Impletacija metoda i dodatnih okolnosti

--- main.py
+++ main.py
@@ -21,21 +21,21 @@ class KorisnickiNalog:
         Vraća trenutno stanje na računu.
         Implementirajte povrat vrijednosti privatnog atributa __stanje_na_racunu.
         """
-        pass
+        return self.__stanje_na_racunu
 
     def get_dugovanja(self):
         """
         Vraća trenutna dugovanja korisnika.
         Implementirajte povrat vrijednosti privatnog atributa __dugovanja.
         """
-        pass
+        return self.__dugovanja
 
     def set_dugovanja(self, dug):
         """
         Postavlja novu vrijednost dugovanja.
         Implementirajte logiku za ažuriranje privatnog atributa __dugovanja.
         """
-        pass
+        self.__dugovanja = dug 
 
     # Metode za manipulaciju računom
     def uplata(self, iznos):
@@ -43,7 +43,8 @@ class KorisnickiNalog:
         Uplaćuje određeni iznos na račun.
         Implementirajte provjeru da li je iznos veći od 0, a zatim dodajte iznos na __stanje_na_racunu.
         """
-        pass
+        if iznos > 0:
+            self.__stanje_na_racunu += iznos
 
     def isplata(self, iznos):
         """
@@ -51,21 +52,23 @@ class KorisnickiNalog:
         Implementirajte provjeru da li je iznos veći od 0 i da li ima dovoljno sredstava na računu.
         Ako su uvjeti ispunjeni, smanjite __stanje_na_racunu.
         """
-        pass
+        if iznos > 0 and iznos <= self.__stanje_na_racunu:
+            self.__stanje_na_racunu -= iznos
 
     def provjeri_stanje(self):
         """
         Provjerava trenutno stanje na računu.
         Implementirajte ispis trenutnog stanja na računu u konzolu.
         """
-        pass
+        print(f"Trenutno stanje računa je {self.__stanje_na_racunu} KM")
 
     def dodaj_dugovanje(self, iznos):
         """
         Dodaje dugovanje korisniku.
         Implementirajte provjeru da li je iznos veći od 0, a zatim povećajte vrijednost __dugovanja.
         """
-        pass
+        if iznos > 0:
+            self.__dugovanja += iznos
 
     def otplati_dugovanje(self, iznos):
         """
@@ -73,7 +76,8 @@ class KorisnickiNalog:
         Implementirajte provjeru da li je iznos validan (veći od 0 i manji ili jednak trenutnom dugu),
         a zatim umanjite __dugovanja.
         """
-        pass
+        if iznos > 0 and iznos <= self.__dugovanja:
+            self.__dugovanja -= iznos
 
     # Metode za ažuriranje podataka korisnika
     def promijeni_broj_telefona(self, novi_broj):
@@ -81,14 +85,14 @@ class KorisnickiNalog:
         Ažurira broj telefona korisnika.
         Implementirajte logiku za promjenu atributa broj_telefona.
         """
-        pass
+        self.broj_telefona = novi_broj
 
     def promijeni_email(self, novi_email):
         """
         Ažurira email adresu korisnika.
         Implementirajte logiku za promjenu atributa email.
         """
-        pass
+        self.email = novi_email
 
     # Metode za pregled informacija
     def prikazi_podatke(self):
@@ -96,11 +100,13 @@ class KorisnickiNalog:
         Ispisuje osnovne informacije o korisniku.
         Implementirajte ispis svih atributa korisnika, uključujući privatne atribute.
         """
-        pass
+        print(f"Ime i prezime: {self.ime_prezime}\nGodište: {self.godiste}\nUlica: {self.ulica}\nBroj telefon: {self.broj_telefona}\nEmail: {self.email}\nStanje na racunu: {self.__stanje_na_racunu}\nDugovanje: {self.__dugovanja}")
 
     def je_li_duzan(self):
         """
         Provjerava da li korisnik ima dugovanja.
         Implementirajte povrat True ako su dugovanja veća od 0, inače False.
         """
-        pass
+        return True if self.__dugovanja > 0 else False
+
+
